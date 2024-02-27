4a41aa3523329c881982fcf28545887f441f0623
sana-sk <sana.sheikh@zensar.com>
2024-02-27 08:53:30 +0530

    first commit

---
+++ Projects/first/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return(
+    <div className='App'>
+      <img 
+      src='https://occ-0-2085-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABW0CdL7BuY0VTI81TjA2iMRBryIJt2mUIsehZnbVH_AlD0DAXB5kE9-RJ9f-at0-SPB4PA8QbonFUC3eA-ND3puHO2m-7pab-mCDUA1QTbPbw89q3EkQfjcY9M4NM0rZEnGE.jpg?r=4f5' alt='cj'/>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ Projects/first/src/BookList.jsx
@@ -0,0 +1,8 @@
+export function BookList() {
+    return (
+        <div>
+            <h2>Hi</h2>
+            <p>Lorem</p>
+        </div>
+    )
+}
